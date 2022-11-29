589ed2460f9cb9d3588300d4471af0312fdbcdbf
Perdana Main <firman.fp123@gmail.com>
2022-11-30 00:02:46 +0700

    Commit before merge

--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -10,7 +10,7 @@ const Promotion = () => {
               Terbang <span>Lebih Hemat</span> ke berbagai destinasi
             </p>
             <p className="flit-tagline">#easy way to flight</p>
-            <a className="btn-tagline">
+            <a href="/" className="btn-tagline">
               <button className="btn btn-primary ">Info Lebih Lanjut</button>
             </a>
           </div>
@@ -18,7 +18,10 @@ const Promotion = () => {
             <div className="container mt-5">
               <div className="text-center">
                 <h1>BEST DEAL</h1>
-                <p>Harga hemat dan potongan tambahan hingga Rp. 400.000 hanya di 10 Desember 2022 hingga 31 Januari 2023</p>
+                <p>
+                  Harga hemat dan potongan tambahan hingga Rp. 400.000 hanya di
+                  10 Desember 2022 hingga 31 Januari 2023
+                </p>
               </div>
               <div>
                 <img src="./assets/landing/Promo.svg" alt="" />
