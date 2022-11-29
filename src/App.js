fa057daa462f68ba5334477616c7081a23e029be
Perdana Main <firman.fp123@gmail.com>
2022-11-29 12:12:44 +0700

    sliced 404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./page/Landing";
 import Login from "./page/Login";
 import Regist from "./page/Register";
+import NotFound from "./page/generateNotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/regist" element={<Regist />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
