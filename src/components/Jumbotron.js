b5b4938e935c2f1ee13863417cb463a57f24c597
unknown <ahmad.19058.mhs.unesa.ac.id>
2022-12-01 15:36:05 +0700

    Update Responsive

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -5,6 +5,7 @@ const Jumbotron = () => {
   return (
     <section id="home">
       <div className="jumbotron background-image">
+        <img src="./assets/landing/pesawat.svg" alt="flit" width="100%" />
         <p className="text-center">Easy Way to FLight</p>
       </div>
     </section>
