8f32467416b3d9730c1045c56a8b998e7e0212d5
Perdana Main <firman.fp123@gmail.com>
2022-12-04 16:06:13 +0700

    merge from master

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Landing from "./components/Landing";
-import FormLogin from "./components/Login";
-import FormRegister from "./components/Register";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
 import NotFound from "./components/notFound";
 
@@ -12,8 +12,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<FormLogin />} />
-      <Route path="/regist" element={<FormRegister />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/regist" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/404" element={<NotFound />} />
       <Route path="*" element={<Navigate to="/404" />} />
