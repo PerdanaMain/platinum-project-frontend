6cd401252b26332fbe14bc857f758e8f8c91bf1b
Perdana Main <firman.fp123@gmail.com>
2022-12-06 09:01:44 +0700

    sprint 2 initial commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
+import Sidebar from "../components/Sidebar";
+const Dashboard = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [name, setName] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    decoder();
+  });
+
+  const decoder = () => {
+    try {
+      const accessToken = sessionStorage.getItem("accessToken");
+      if (!accessToken) {
+        navigate("/404");
+      }
+      const decoded = jwt_decode(accessToken);
+      setFirstName(decoded.firstname);
+      setLastName(decoded.lastname);
+      setName(firstName + " " + lastName);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="container-dash">
+      <Sidebar />
+      <div class="main-content">
+        <h1 className="m-3">Welcome, {name}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
