f69c1fd990779452a4d2da0a32259975ac35a046
unknown <ahmad.19058.mhs.unesa.ac.id>
2022-12-13 23:15:24 +0700

    Update Form Data

---
+++ src/components/BookingData.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function BookingData() {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-body">
+          <div class="container text-center">
+            <div class="row align-items-start">
+              <div class="col"></div>
+              <div class="col">One of three columns</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BookingData;
